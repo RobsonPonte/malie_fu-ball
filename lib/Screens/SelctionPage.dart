a9cd369519d3dcdf318bf81641c867eaea637c05
RobsonPonte <robert_la@web.de>
2021-01-15 16:09:47 +0100

    new

--- lib/Screens/SelctionPage.dart
+++ lib/Screens/SelctionPage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:malie/widgets/BasisSeite.dart';
 
 class SelectionPage extends StatelessWidget {
   @override
@@ -7,79 +6,81 @@ class SelectionPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Flexible(fit: FlexFit.tight, child: BasisSeite()),
-          Flexible(
-            fit: FlexFit.tight,
-            child: Column(
-              children: [
-                Text(
-                  'I´m a:',
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                  textAlign: TextAlign.center,
+          Container(height: 10),
+          Container(
+            height: 250,
+            child: Image.asset(
+              'images/logo_schrift.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Text(
+            'I´m a:',
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            width: 280,
+            child: RaisedButton(
+              onPressed: () {},
+              //Funktion button muss noch implementiert werden
+              child: Text(
+                'athlete',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
                 ),
-                SizedBox(
-                  width: 280,
-                  child: RaisedButton(
-                    onPressed: () {},
-                    //Funktion button muss noch implementiert werden
-                    child: Text(
-                      'athlete',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                      ),
-                    ),
-                    color: Color.fromRGBO(14, 209, 149, 1),
-                    elevation: 10.0,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                  ),
+              ),
+              color: Color.fromRGBO(14, 209, 149, 1),
+              elevation: 10.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 280,
+            child: RaisedButton(
+              onPressed: () {},
+              //Funktion button muss noch implementiert werden
+              child: Text(
+                'club',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
                 ),
-                SizedBox(
-                  width: 280,
-                  child: RaisedButton(
-                    onPressed: () {},
-                    //Funktion button muss noch implementiert werden
-                    child: Text(
-                      'club',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                      ),
-                    ),
-                    color: Color.fromRGBO(12, 160, 183, 1),
-                    elevation: 10.0,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                  ),
+              ),
+              color: Color.fromRGBO(12, 160, 183, 1),
+              elevation: 10.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 280,
+            child: RaisedButton(
+              onPressed: () {},
+              //Funktion button muss noch implementiert werden
+              child: Text(
+                'staff',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
                 ),
-                SizedBox(
-                  width: 280,
-                  child: RaisedButton(
-                    onPressed: () {},
-                    //Funktion button muss noch implementiert werden
-                    child: Text(
-                      'staff',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                      ),
-                    ),
-                    color: Color.fromRGBO(8, 86, 133, 1),
-                    elevation: 10.0,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                  ),
-                ),
-              ],
+              ),
+              color: Color.fromRGBO(8, 86, 133, 1),
+              elevation: 10.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
             ),
           )
         ],
