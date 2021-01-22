2ebddaf73fc4fe3e3e42f0c30e9aaa4970299ba9
RobsonPonte <robert_la@web.de>
2021-01-22 16:04:13 +0100

    new

---
+++ lib/Data/personal_data.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+/* 
+const PERSONAL_CATEGORIES = const [
+  Category(
+    id: 'p1',
+    title: 'trainer',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'p2',
+    title: 'spielertrainer',
+    color: Color.fromRGBO(14, 209, 149, 1),
+  ),
+  Category(
+    id: 'p3',
+    title: 'co-trainer',
+    color: Color.fromRGBO(14, 209, 149, 1),
+  ),
+  Category(
+    id: 'p4',
+    title: 'krafttrainer',
+    color: Color.fromRGBO(14, 209, 149, 1),
+  ),
+  Category(
+    id: 'p5',
+    title: 'torwarttrainer',
+    color: Color.fromRGBO(12, 160, 183, 1),
+  ),
+  Category(
+    id: 'p6',
+    title: 'fitnesstrainer',
+    color: Color.fromRGBO(12, 160, 183, 1),
+  ),
+  Category(
+    id: 'p7',
+    title: 'betreuer',
+    color: Color.fromRGBO(12, 160, 183, 1),
+  ),
+  Category(
+    id: 'p8',
+    title: 'physiotherapeut',
+    color: Color.fromRGBO(12, 160, 183, 1),
+  ),
+  Category(
+    id: 'p9',
+    title: 'jugendtrainer',
+    color: Color.fromRGBO(12, 160, 183, 1),
+  ),
+  Category(
+    id: 'p10',
+    title: 'arzt',
+    color: Color.fromRGBO(8, 86, 133, 1),
+  ),
+  Category(
+    id: 'p11',
+    title: 'sportdirektor',
+    color: Color.fromRGBO(8, 86, 133, 1),
+  ),
+]; */
---
+++ lib/Data/verein_data.dart
@@ -0,0 +1,25 @@
+/* import 'package:flutter/material.dart';
+
+
+const VEREIN_CATEGORIES = const [
+  Category(
+    id: 'v1',
+    title: '1. abteilungsleiter',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'v2',
+    title: '2. abteilungsleiter',
+    color: Color.fromRGBO(14, 209, 149, 1),
+  ),
+  Category(
+    id: 'v3',
+    title: 'sportdirektor',
+    color: Color.fromRGBO(14, 209, 149, 1),
+  ),
+  Category(
+    id: 'v4',
+    title: 'vereinsverwalter',
+    color: Color.fromRGBO(14, 209, 149, 1),
+  ),
+  ]; */
