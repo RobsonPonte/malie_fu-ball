69e7ecaf81750356103f4c2b257d13b4679b25d5
RobsonPonte <robert_la@web.de>
2021-01-23 11:00:17 +0100

    new

--- lib/Positionen_screens/Verein_screen.dart
+++ lib/Positionen_screens/Verein_screen.dart
@@ -23,26 +23,35 @@ class PositionVerein extends StatelessWidget {
               style: TextStyle(fontSize: 22),
             ),
           ),
+          Container(
+            height: 130,
+            margin: EdgeInsets.only(right: 15, left: 15, top: 15),
+            child: GridView(
+              physics: NeverScrollableScrollPhysics(),
+              children: VEREIN_POSITIONEN
+                  .map(
+                    (catData) => AllgemeinItem(
+                      catData.title,
+                      catData.color,
+                    ),
+                  )
+                  .toList(),
+              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 150,
+                childAspectRatio: 5 / 1,
+                crossAxisSpacing: 15,
+                mainAxisSpacing: 15,
+              ),
+            ),
+          ),
           Expanded(
             child: Container(
-              height: 160,
-              margin: EdgeInsets.only(right: 15, left: 15, top: 15),
-              child: GridView(
-                physics: NeverScrollableScrollPhysics(),
-                children: VEREIN_POSITIONEN
-                    .map(
-                      (catData) => AllgemeinItem(
-                        catData.title,
-                        catData.color,
-                      ),
-                    )
-                    .toList(),
-                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                  maxCrossAxisExtent: 150,
-                  childAspectRatio: 5 / 1,
-                  crossAxisSpacing: 15,
-                  mainAxisSpacing: 15,
-                ),
+              alignment: Alignment.topLeft,
+              margin: EdgeInsets.only(top: 10, right: 15, left: 20, bottom: 15),
+              //color: Colors.red,
+              child: Text(
+                '*die oben genannten Positionen können nur einmal pro Verein besetzt werden.',
+                style: TextStyle(fontSize: 15),
               ),
             ),
           ),
